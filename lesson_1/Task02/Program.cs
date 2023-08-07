a9d63a069da23a9f499c038b48106141710cd9d1
Простакишин Сергей Игоревич <prostakishin-s@POKRMINE.RU>
2023-08-07 13:39:55 +0900

    Закончил со вторым заданием

--- lesson_1/Task02/Program.cs
+++ lesson_1/Task02/Program.cs
@@ -2,3 +2,16 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine($"Число {num1} является наибольшим");
+}
+else{
+    Console.WriteLine($"Число {num2} является наибольшим");
+}
