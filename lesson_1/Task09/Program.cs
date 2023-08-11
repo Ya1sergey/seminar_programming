b865bb93351aee218772c8ab41649ea29d46aeb2
Простакишин Сергей Игоревич <prostakishin-s@POKRMINE.RU>
2023-08-11 13:09:52 +0900

    9 и 11 задачи

---
+++ lesson_1/Task09/Program.cs
@@ -0,0 +1,37 @@
+﻿// 9. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+
+int number = new Random().Next(10, 100); // 99 + 1
+Console.WriteLine($"Случайное число из отррезка 10 - 99 -> {number}");
+
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+
+// if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
+
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit; //Тернарный оператор
+// Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int result = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа -> {result}");
+
+int result1 = MaxDigit(12);
+Console.WriteLine($"Наибольшая цифра числа -> {result1}");
+
+int result2 = MaxDigit(23);
+Console.WriteLine($"Наибольшая цифра числа -> {result2}");
+
+int MaxDigit(int num) // num = number
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return maxDigit;
+}
---
+++ lesson_1/Task11/Program.cs
@@ -0,0 +1,18 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+int result = MinusDigit2(number);
+Console.WriteLine($"{number} -> {result}");
+
+int MinusDigit2(int num)
+{
+    int Digit1 = num / 100;
+    int Digit3 = num % 10;
+
+    return Digit1 * 10 + Digit3;
+}
