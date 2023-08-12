ebeed7e9b31b9335beb1883386736a6fe1acce85
Простакишин Сергей Игоревич <prostakishin-s@POKRMINE.RU>
2023-08-12 15:42:52 +0900

    13 задача

---
+++ lesson_1/Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num <= 100) Console.WriteLine("Третьей цифры нет");
+
+else Console.WriteLine($"Третья цифра числа: {Digit3(num)}");
+
+int Digit3(int number)
+{
+    while (number >= 1000)
+    {
+        number = number / 10;
+    }
+    return number % 10;
+}
