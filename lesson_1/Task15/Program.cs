baebae44eff14afa3bd405fad90ab1bd824162ae
Простакишин Сергей Игоревич <prostakishin-s@POKRMINE.RU>
2023-08-12 16:27:58 +0900

    15 задача доработал

--- lesson_1/Task15/Program.cs
+++ lesson_1/Task15/Program.cs
@@ -2,13 +2,32 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
- 
-Console.WriteLine("Введите номер дня недели");
-int numWeek = Convert.ToInt32(Console.ReadLine());
 
-if(numWeek == 6 || numWeek == 7) Console.WriteLine("Да");
-else 
+Console.WriteLine("Введите номер дня недели");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+if (Weekday(num))
 {
-    if(numWeek > 7 || numWeek < 1) Console.WriteLine("Вы ввели неверный номер недели");
-    else Console.WriteLine("Нет");
+    if (Weekend(num))
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+        Console.WriteLine("Рабочий день");
+    }
+}
+
+bool Weekend(int numWeek)
+{
+    if (numWeek == 6 || numWeek == 7) return true;
+    return false;
+}
+
+bool Weekday(int number)
+{
+    if (number >= 1 && number <= 7) return true;
+    Console.WriteLine("Вы ввели неверный номер недели");
+    return false;
 }
