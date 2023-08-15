bbddaed9ac5504bbb92ee58fdcec99777b26d52b
Простакишин Сергей Игоревич <prostakishin-s@POKRMINE.RU>
2023-08-15 20:01:18 +0900

    17, 18, 20 задания

---
+++ lesson_1/Task18/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите номер четверти: ");
+string num = Console.ReadLine();
+
+Console.WriteLine(Quarter(num));
+
+string Quarter(string number)
+{
+    if (number == "1") return $"Диапазон четверти {number}: X > 0, Y > 0";
+    if (number == "2") return $"Диапазон четверти {number}: X < 0, Y > 0";
+    if (number == "3") return $"Диапазон четверти {number}: X < 0, Y < 0";
+    if (number == "4") return $"Диапазон четверти {number}: X < 0, Y < 0";
+    return "Вы ввели неверный номер четверти";
+}
---
+++ lesson_1/Task20/Program.cs
@@ -0,0 +1,34 @@
+﻿// // double d = 5.099864545;
+// // double dRound = Math.Round(d, 2, MidpointRounding.ToZero);
+// // Console.WriteLine(dRound);
+
+// // double sqrt = Math.Sqrt(5);
+
+// Задача 20: Напишите программу, которая
+// принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D
+// пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.WriteLine("Введите координаты точки");
+Console.Write("X1: ");
+int x1Coordinate = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y1: ");
+int y1Coordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки");
+Console.Write("X2: ");
+int x2Coordinate = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y2: ");
+int y2Coordinate = Convert.ToInt32(Console.ReadLine());
+
+double distance = DistanceXY(x1Coordinate, x2Coordinate, y1Coordinate, y2Coordinate);
+double distanceRound = Math.Round(distance, 2, MidpointRounding.ToZero);
+Console.WriteLine($"Расстояния между точками: {distanceRound}");
+
+double DistanceXY(int x1, int x2, int y1, int y2)
+{
+    double result = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    return result;
+}
