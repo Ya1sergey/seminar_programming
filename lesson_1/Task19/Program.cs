122a09eed51bbb4630bb8d600855d3779415da75
Простакишин Сергей Игоревич <prostakishin-s@POKRMINE.RU>
2023-08-18 09:28:21 +0900

    19 задание. Ошибка при перевороте

---
+++ lesson_1/Task19/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 10000 && number <= 99999 || number <= -10000 && number >= -99999)
+    Palindrome(number);
+else
+{
+    Console.WriteLine("Вы ввели не пятизначное число");
+    return;
+}
+
+void Palindrome(int num)
+{
+    int palindrome = 0;
+    if (num < 0)
+    {
+        for (int result = num; palindrome >= result; num = num / 10)
+        {
+            palindrome = palindrome * 10 + num % 10;
+
+        }
+    }
+    else
+    {
+        for (int result = num; palindrome <= result; num = num / 10)
+        {
+            palindrome = palindrome * 10 + num % 10;
+            Console.WriteLine(palindrome);
+        }
+    }
+    if ((palindrome = palindrome % 10) != 0)
+        Console.WriteLine("Нет");
+    else Console.WriteLine("Да");
+}
+
